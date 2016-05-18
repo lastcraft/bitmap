4abcdf0cb8188ad5e08404511d1dd45b76372c22
Marcus Baker <marcus@lastcraft.com>
2016-05-18 18:24:41 +0100

    ALl in one method prior to refactor

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,24 +1,18 @@
 require_relative '../lib/editor'
 
 class BitmapEditor
-  def initialize
-    @editor = Editor.new
-    @editor.on_exit { exit_console }
-  end
-
   def run
+    editor = Editor.new
+    editor.on_exit do
+      puts 'goodbye!'
+      @running = false
+    end
     @running = true
     puts 'type ? for help'
     while @running
       print '> '
       input = gets.chomp
-      puts @editor.parse(input)
+      puts editor.parse(input)
     end
   end
-
-  private
-    def exit_console
-      puts 'goodbye!'
-      @running = false
-    end
 end
