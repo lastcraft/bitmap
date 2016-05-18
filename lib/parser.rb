715666cdf62c43c81659d370a880234b83301e23
Marcus Baker <marcus@lastcraft.com>
2016-05-18 21:13:16 +0100

    Elelentary parsing

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,7 +1,26 @@
 class NoMatch < RuntimeError; end
 
 class Parser
+  def initialize
+    @patterns = []
+  end
+
   def parse(command)
+    each_pattern do |pattern, callback|
+      if result = pattern.match(command)
+          return callback.call(*result[1..-1]) if callback
+      end
+    end
     raise NoMatch
+  end
+  
+  def on(pattern, &callback)
+    @patterns << [pattern, callback]
+    self
+  end
+  
+  private
+  def each_pattern
+    @patterns.each {|pair| yield *pair }
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,7 +2,19 @@ require 'rspec'
 require_relative '../lib/parser'
 
 describe Parser do
-  it "raises if no match" do
+  it "raises if nothing to match" do
     expect { Parser.new.parse('') }.to raise_error NoMatch
+  end
+  
+  it "raises if fails to match" do
+    expect { Parser.new.on(/Z/) { }.parse('') }.to raise_error NoMatch
+    expect { Parser.new.on(/Z/) { }.parse('A') }.to raise_error NoMatch
+    expect { Parser.new.on(/^Z/) { }.parse(' Z') }.to raise_error NoMatch
+  end
+  
+  it "triggers callback on exact match" do
+    z = false
+    Parser.new.on(/Z/) { z = true }.parse('Z')
+    expect(z).to be_truthy
   end
 end
